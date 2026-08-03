8e5f5403f1d700d6a31205c6e09874f1829ab678
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-08-03 16:34:51 +0530

    Create Day1TheoryIntro.java

---
+++ Day1TheoryIntro.java
@@ -0,0 +1,14 @@
+package TheoryClass;
+
+public class Day1Task1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi");
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+
+	}
+
+}
